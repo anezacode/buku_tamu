47e6461c3197a8edc617f2932a5f2cf53b4fb51f
AnezaCode <156332330+anezacode@users.noreply.github.com>
2024-11-07 21:53:46 +0700

    Update and rename input_process.php to process.php

--- process.php
+++ process.php
@@ -1,20 +1,20 @@
 <?php
-include_once('connection.php');
+require_once 'connection.php';
 
 if (isset($_POST['role'])) {
     $status = $_POST['role'];
 
     if ($status == "SISWA") {
-        $username = strtoupper($_POST['username-siswa']);
+        $username = htmlspecialchars(strtoupper($_POST['username-siswa']));
         $grade = $_POST['level'] . ' ' . $_POST['jurusan'] . ' ' . $_POST['nomor'];
         $message = $_POST['input-kp1'];
         $query = "INSERT INTO tb_student (username, grade, status, message, time) VALUES ('$username', '$grade', '$status', '$message', CURRENT_TIMESTAMP)";
     } elseif ($status == "GURU") {
-        $username = strtoupper($_POST['username-guru']);
+        $username = htmlspecialchars(strtoupper($_POST['username-guru']));
         $jabatan = $_POST['jabatan'];
         $query = "INSERT INTO tb_teacher (username, jabatan, status, time) VALUES ('$username', '$jabatan', '$status', CURRENT_TIMESTAMP)";
     } elseif ($status == "PENGUNJUNG") {
-        $username = strtoupper($_POST['username-pengunjung']);
+        $username = htmlspecialchars(strtoupper($_POST['username-pengunjung']));
         $query = "INSERT INTO tb_visitor (username, status, time) VALUES ('$username', '$status', CURRENT_TIMESTAMP)";
     } else {
         exit;
