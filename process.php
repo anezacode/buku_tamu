00f1d24d095b3564ed4adad0e6297583df88e493
AnezaCode <156332330+anezacode@users.noreply.github.com>
2024-11-11 19:14:25 +0700

    Update process.php

--- process.php
+++ process.php
@@ -11,21 +11,24 @@ if (isset($_POST['role'])) {
         $query = "INSERT INTO tb_student (username, grade, status, message, time) VALUES ('$username', '$grade', '$status', '$message', CURRENT_TIMESTAMP)";
     } elseif ($status == "GURU") {
         $username = htmlspecialchars(strtoupper($_POST['username-guru']));
-        $jabatan = $_POST['jabatan'];
-        $query = "INSERT INTO tb_teacher (username, jabatan, status, time) VALUES ('$username', '$jabatan', '$status', CURRENT_TIMESTAMP)";
+        $jabatan = htmlspecialchars(strtoupper($_POST['jabatan']));
+        $message = $_POST['input-kp2'];
+        $query = "INSERT INTO tb_teacher (username, jabatan, status, message, time) VALUES ('$username', '$jabatan', '$status', '$message', CURRENT_TIMESTAMP)";
     } elseif ($status == "PENGUNJUNG") {
         $username = htmlspecialchars(strtoupper($_POST['username-pengunjung']));
-        $query = "INSERT INTO tb_visitor (username, status, time) VALUES ('$username', '$status', CURRENT_TIMESTAMP)";
+        $institute = htmlspecialchars(strtoupper($_POST['asal-instansi']));
+        $message = $_POST['input-kp3'];
+        $query = "INSERT INTO tb_visitor (username, institute, status, message, time) VALUES ('$username', '$institute', '$status', '$message', CURRENT_TIMESTAMP)";
     } else {
         exit;
     }
 
     $hasil = mysqli_query($con, $query);
     if (!$hasil) {
-        echo $log. " KESALAHAN dalam memasukkan data: " . mysqli_error($con);
+        echo $log. " KESALAHAN dalam memproses data:\n" . mysqli_error($con);
         exit;
+    } else {
+        sleep(1);
+        header('location: index.php');
     }
 }
-
-header('location:index.php');
-?>
