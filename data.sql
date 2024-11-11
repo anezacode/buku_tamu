ac65ddef6594b42a1d66dc0aeb6fba4e0089ee52
AnezaCode <156332330+anezacode@users.noreply.github.com>
2024-11-11 19:14:10 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -18,6 +18,7 @@ CREATE TABLE tb_teacher (
     username VARCHAR(50) NOT NULL,
     jabatan VARCHAR(50) NOT NULL,
     status ENUM('GURU') NOT NULL,
+    message TEXT,
     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -26,5 +27,6 @@ CREATE TABLE tb_visitor (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     status ENUM('PENGUNJUNG') NOT NULL,
+    message TEXT,
     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
